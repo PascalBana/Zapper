593343f6bf6aa352964dfb01f958822ffbf8b245
PascalBana <pascalrey1955@gmail.com>
2024-04-03 20:48:06 -0600

    added background color

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
+        .add_systems(Startup, (hello_world, spawn_camera))
         .run();
 }
 
 fn hello_world() {
     println!("Hello, world!");
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        camera: Camera {
+            clear_color: ClearColorConfig::Custom(Color::rgb(0.8, 0.6, 0.7)),
+            ..default()
+        },
+        ..default()
+    });
 }
