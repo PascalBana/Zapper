8fb9624c3d7ffeaa77e10ad9dbb84fc462792436
JC <secret@example.com>
2024-04-04 03:01:51 +0000

    feat(nixops): added nix development support

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          # 👇 note that it refers to the path ./rust-toolchain.toml
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+            rustToolchain
+            ];
+	    packages = with pkgs; [ wayland-utils libudev-zero alsa-lib pkg-config ]; 
+	    shellHook = ''
+		echo "Loading Rust development environment"
+            '';
+          };
+        }
+      );
+}
