f36e8675e5974ed9b44e9c55ca6cf5744866a0d9
JCGonzalez <john@selkirk.com>
2024-04-04 17:02:07 -0700

    feat!(nixops): conditionalAttrs, darwin.Stdenv

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  description = "Rust + Bevy development flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -19,20 +20,32 @@
           pkgs = import nixpkgs {
             inherit system overlays;
           };
-          # 👇 note that it refers to the path ./rust-toolchain.toml
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+          # Define macOS-specific packages
+          darwinPackages = pkgs.lib.optionalAttrs (pkgs.stdenv.isDarwin) (with pkgs.darwin.apple_sdk.frameworks; {
+            additionalPackages = [ Security CoreServices CoreFoundation Foundation AppKit ];
+          });
+
+          # Define Linux-specific packages
+          linuxPackages = pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+            additionalPackages = with pkgs; [ libudev-zero alsa-lib pkg-config ];
+          };
+
         in
         with pkgs;
         {
           devShells.default = mkShell {
             buildInputs = [
-            rustToolchain
-            ];
-	    packages = with pkgs; [ wayland-utils libudev-zero alsa-lib pkg-config ]; 
-	    shellHook = ''
-		echo "Loading Rust development environment"
+              rustToolchain
+            ] ++ (linuxPackages.additionalPackages or []) # Include Linux-specific packages if on Linux
+            ++ (darwinPackages.additionalPackages or []); # Include macOS-specific packages if on Darwin
+            shellHook = ''
+              export NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1
+              echo "Loading Rust development environment"
             '';
           };
         }
       );
 }
+
